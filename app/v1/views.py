8a7ac8c83a8f8889b300a08d0e5098e569238755
nimeshkverma <nimesh.aug11@gmail.com>
2016-01-19 14:45:21 +0530

    Correcting the Code

--- app/v1/views.py
+++ app/v1/views.py
@@ -3,26 +3,4 @@ from utils import *
 
 
 def query_solr():
-    # print request.json()
-    return request.get_json(force=True)
-
-#     response = get_response(request)
-#     return jsonify(response)
-
-
-# def api_response(api_request, api_mappings):
-#     api = api_request.path.split('/')[-1]
-#     api_uri = api_mappings[api]["third_party_url"]
-#     payload = {}
-
-#     if api_request.method == 'POST':
-#         for param in api_mappings[api]["params"]:
-#             payload[param] = api_request.form[param]
-#         api_response = requests.post(api_uri, data=payload)
-#     else:
-#         for param in api_mappings[api]["params"]:
-#             payload[param] = api_request.args.get(param)
-#         api_response = requests.get(api_uri, params=payload)
-
-#     api_response.raise_for_status()
-#     return api_response.json()
+    return jsonify(request.json)
