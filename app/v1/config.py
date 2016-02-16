3c639e64d12d6a07478c72ff15c8c6a6a847b1ab
Shivam Aggarwal <shivama205@gmail.com>
2016-02-16 18:03:17 +0530

    Added parser for filters in input json

---
+++ app/v1/config.py
@@ -0,0 +1,12 @@
+Datatype = {
+	'ARRAY': 'array',
+	'DICTIONARY': 'json',
+}
+
+FILTER_KEY = 'filters'
+TYPE = 'type'
+VALUE = 'value'
+IS_NOT = 'not_query'
+FIELDS = 'fields'
+MIN_VALUE = 'min'
+MAX_VALUE = 'max'
--- app/v1/views.py
+++ app/v1/views.py
@@ -4,9 +4,7 @@ import json
 from mysolr import Solr
 
 def query_solr():
-	j = json.loads(request.data)
-	query_string = parse_json(j)
-	print query_string
+	query_string = parse_json(json.loads(request.data))
 
 	solr = Solr('http://52.76.188.127:8983/solr/clickstream_event_shard1_replica1/')
 	solr_response = solr.search(q=query_string)
